58ddae2dd91ce2db00cafad15e07f888fa5cd836
Kasper <ofek4430@gmail.com>
2020-12-24 15:25:29 +0200

    Prevent a crash when led_names length doesn't match the leds count

--- Controllers/QMKRGBMatrixController/RGBController_QMKRGBMatrix.cpp
+++ Controllers/QMKRGBMatrixController/RGBController_QMKRGBMatrix.cpp
@@ -504,7 +504,7 @@ void RGBController_QMKRGBMatrix::SetupZones()
         {
             led keyboard_led;
 
-            if(leds.size() < zones[0].leds_count) 
+            if(leds.size() < zones[0].leds_count && led_idx < led_names.size()) 
                 keyboard_led.name = "Key: " + led_names[led_idx];
             else 
                 keyboard_led.name = "Underglow: " + std::to_string(led_idx);
